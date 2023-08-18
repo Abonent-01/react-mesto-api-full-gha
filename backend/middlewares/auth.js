d0d98e385747308afd898a8ea67b726a7554fd3d
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-19 01:11:57 +1000

    15

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,22 +2,22 @@ const jwt = require('jsonwebtoken');
 const ERROR_CODE_AUTH = require('../error/authError');
 
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
-
-  if (!token) {
-    return next(new ERROR_CODE_AUTH('Error...'));
+  const { authorization } = req.headers;
+  if (!authorization) {
+    next(new ERROR_CODE_AUTH('Error... Who auth'));
+    return;
   }
 
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, 'SECRET');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret')
   } catch (err) {
-    return next(new ERROR_CODE_AUTH('Error...'));
+    next(new ERROR_CODE_AUTH('Error... auth'));
+    return;
   }
-
   req.user = payload;
   next();
-};
+}
 
 module.exports = auth;
